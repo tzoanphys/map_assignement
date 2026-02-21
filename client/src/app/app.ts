0bebd02676d6778c6be47c0b50a0ade0816f43b5
tzoanphys <tzoanphys@gmail.com>
2026-02-21 13:50:12 +0100

    Add frontend+openlayers

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,41 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements AfterViewInit {
+
+  map!: Map;
+  lastInfo: string | null = null;
+
+  ngAfterViewInit(): void {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([4.3517, 50.8503]), // Brussels
+        zoom: 11,
+      }),
+    });
+  }
+
+  setDrawMode(type: 'LineString' | 'Polygon') {
+    console.log("Draw mode:", type);
+  }
+
+  stopDrawing() {
+    console.log("Stop drawing");
+  }
 }
